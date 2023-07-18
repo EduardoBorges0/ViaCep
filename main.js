a908c2983c6a703ba57425b3733c8f128eb11ec7
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2023-07-18 17:56:07 -0300

    arrumando a invalidação

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 function confirm(a) {
     a.preventDefault()
     const select = document.querySelector('.Estado');
-    const option = select.value = select.value;
     const localidade = document.querySelector('.Localidade');
     const logradouro = document.querySelector('.Cidade')
     const cep = document.querySelector('.Cep')
-    fill(option, localidade, logradouro, cep)
+    fill(select, localidade, logradouro, cep)
 }
-async function fill(option, localidade, logradouro, cep) {
-    const url = `https://viacep.com.br/ws/${option}/${localidade.value}/${logradouro.value}/json//`
-    const api = await fetch(url)
-    const response = await api.json()
-    if (response.length == 0) {
-        cep.value = 'CEP não encontrado!'
-        localidade.value = "";
-        logradouro.value = "";
+async function fill(select, localidade, logradouro, cep) {
+    const url = `https://viacep.com.br/ws/${select.value}/${localidade.value}/${logradouro.value}/json//`
+    if (logradouro.value.length > 3) {
+        const api = await fetch(url)
+        const response = await api.json()
+        if (response.length == 0) {
+            animation(cep)
+            logradouro.classList.remove('invalid')
+            localidade.classList.remove('invalid')
+            cep.value = 'CEP não encontrado!'
+            localidade.value = "";
+            logradouro.value = "";
+
+        }
+        else {
+            animation(cep)
+            cep.value = response[0].cep
+            logradouro.classList.remove('invalid')
+            localidade.classList.remove('invalid')
+        }
     }
     else {
-        animation(cep)
-        cep.value = response[0].cep
+        logradouro.classList.add('invalid')
     }
-    console.log(response[0].cep)
+    if (localidade.value.length < 3) {
+        localidade.classList.add('invalid')
+    }
+
 }
 function animation(cep) {
     cep.classList.add('animation')
