7c6e962004c715380629458b8fafa03b27553f6b
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2023-07-21 15:30:32 -0300

    Update loading.js

--- loading.js
+++ loading.js
@@ -7,18 +7,11 @@ function ShowLoading() {
     const cidade = document.querySelector('.Cidade');
     const localidade = document.querySelector('.Localidade')
     const btn = document.querySelector('#btn')
-    localidade.classList.add('disabled')
-    cidade.classList.add('disabled')
-    select.classList.add('disabled')
-    btn.classList.add('disabled')
-    block.classList.add('blockBackground')
-    loading.classList.remove('hide')
-    setTimeout(() => {
-        localidade.classList.remove('disabled')
-        cidade.classList.remove('disabled')
-        select.classList.remove('disabled')
-        block.classList.remove('blockBackground')
-        btn.classList.remove('disabled')
-        loading.classList.add('hide')
-    }, 1280)
+    localidade.classList.toggle('disabled')
+    cidade.classList.toggle('disabled')
+    select.classList.toggle('disabled')
+    btn.classList.toggle('disabled')
+    block.classList.toggle('blockBackground')
+    loading.classList.toggle('hide')
+   
 }
